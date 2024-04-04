053e2ac4b28da49ecfa0e876fc8516f09e03bce2
Willy Samuel Paz Colque <willypaz243@gmail.com>
2024-04-03 20:55:47 -0400

    fix requirements.txt file

--- app/api/main.py
+++ app/api/main.py
@@ -4,10 +4,6 @@ from app.api.routes import api_route
 
 app = FastAPI()
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
     "https://abmodelis.github.io",
 ]
 app.add_middleware(
