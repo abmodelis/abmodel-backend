544a5a3206c82ebf048f056dda6884cc89e1e688
julio <juliocesarsevericheorellana@gmail.com>
2024-05-20 00:25:51 -0400

    Update crud of the contents endpoints | add title

--- app/core/use_cases/contents/schemas/__init__.py
+++ app/core/use_cases/contents/schemas/__init__.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class ContentIn(BaseModel):
+    title: str = Field(min_length=1, max_length=255, description="Titulo de la clase/contenido", examples=["Introducción"])
     html_text: str = Field(min_length=0, description="Contenido del contenido", examples=["# Python"])
     media_path: str = Field(min_length=0, description="Ruta de la imagen del contenido", examples=["images/python.png"])
     unit_id: int = Field(description="Id de la unidad", examples=[1])
